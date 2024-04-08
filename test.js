ff1b7ed22535bd46c2dcf8931c268333452c63d5
yihenew <yihenewu89@gmail.com>
2024-04-08 18:42:11 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const fs = require('fs')
+const server = http.createServer((req,res)=>{
+    res.setHeader('Content-Type','text/html')
+    fs.readFile('./view/index.html',(err,data)=>{
+        if(err){
+            console.log(err)
+            res.end()
+        }else{
+            res.write(data)
+            res.end()
+        }
+    })
+})
+
+server.listen(3000,'localhost',()=>{
+    console.log('listen to request')
+})
